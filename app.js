ddf612a6820bef3d10b1c59811fb7c6bc1bc1be9
Roman Njoroge <roman.njoroge@njuguna.com>
2022-08-30 00:21:40 +0300

    Fixed Navbar and implemented landing page for add route

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+// Middleware
+app.use(express.urlencoded({extended:true}))
+
 // Routes
+// Homepage
 app.route('/').get((req, res)=>{
     res.status(200).sendFile(path.join(__dirname + '/web/index.html'))
+})
+
+// Add page
+app.get('/add', (req, res)=>{
+    res.status(200).sendFile(path.join(__dirname + '/web/add.html'))
+})
+app.post('/add', (req, res)=>{
+    const {word_type, sentence, ngeli} = req.body
+    res.status(200).json({'Success': true})
 })
 
 app.get('*', (req, res) =>{
