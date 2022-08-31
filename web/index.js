7b6ff5b0a4cf8cab0e38571540668eb89d723916
Roman Njoroge <roman.njoroge@njuguna.com>
2022-08-31 19:13:07 +0300

    Added support for getting word data from server and displaying on screen dynamically

---
+++ web/index.js
@@ -0,0 +1,12 @@
+// Variables
+const questionWordPhrase = "The word is "
+const testDoc = document.getElementById("pmeaning")
+console.log(testDoc)
+
+// Get the paragraph from the dom
+const questionWord = document.getElementById("question-word")
+console.log(questionWord)
+
+function test() {
+    testDoc.innerText = "Hello World!"
+}
